c9ceeff15ce6d39a2bd2271f48a83185fae02774
JGScarborough <49846504+JGScarborough@users.noreply.github.com>
2019-04-21 19:36:26 -0400

    Create doexiteight.cpp

---
+++ doexiteight.cpp
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**                                                                                                                                                                                                 
+ * This performs exit(8) to test launcher.cpp                                                                                                                                                       
+ * @returns 0 on success                                                                                                                                                                            
+ */
+int main(){
+  exit(8);
+  return 0;
+} // main                                                                                                                                                                                           
+
+
+
