d0ad706dddb432c91e167bc67c1a0de372350465
JGScarborough <49846504+JGScarborough@users.noreply.github.com>
2019-04-24 12:05:13 -0400

    Update tokeit.cpp

--- tokeit.cpp
+++ tokeit.cpp
@@ -12,29 +12,30 @@ using namespace std;
 
 /**                                                                                                                                                                                                 
  * This tokenizes user input in a 2d array. Then, it executes using the input.                                                                                                                      
- * @return 0 on success, nonzero on error                                                                                                                                                           
+ * @return 0 on success                                                                                                                                                                             
  */
 int main(){
   int numArgs = 0;
   char line[512];
-  char* tokens[10];
+  char* tokens[11];
   char* start;
   cin.getline(line, 512);
-  cout << line << endl;
   start = strtok(line, " ");
-  tokens[numArgs] = start;
-  while(start != nullptr){
+  while(start != (char *) NULL){
     tokens[numArgs] = start;
     numArgs++;
-    start = strtok(nullptr, " ");
+    start = strtok((char *) NULL, " ");
   } // while                                                                                                                                                                                        
-
+  tokens[numArgs + 1] = (char *) NULL;
   cout << "done parsing" << endl;
   cout << "num args: " << numArgs << endl;
   for(int i = 0; i < numArgs; i++){
     cout << tokens[i] << endl;
   } // for                                                                                                                                                                                          
   execvp(tokens[0], tokens);
+  perror("error execvp");
   return 0;
 } // main                                                                                                                                                                                           
+
+
 
