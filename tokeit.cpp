9e3e0e6b97616bab40521c51ef2b58a5214efd71
JGScarborough <49846504+JGScarborough@users.noreply.github.com>
2019-04-23 21:15:40 -0400

    Update tokeit.cpp

--- tokeit.cpp
+++ tokeit.cpp
@@ -11,7 +11,8 @@
 using namespace std;
 
 /**                                                                                                                                                                                                 
- *                                                                                                                                                                                                  
+ * This tokenizes user input in a 2d array. Then, it executes using the input.                                                                                                                      
+ * @return 0 on success, nonzero on error                                                                                                                                                                                                  
  */
 int main(){
 int numArgs = 0;
@@ -24,22 +25,18 @@ int numArgs = 0;
   char* cmd = start;
   argv[0] = cmd; 
   while(start != NULL){
-   // printf("%s\n", start);
     numArgs++;
     start = strtok(NULL," ");
     argv[i] = start;
     i++;
   } // while  
-   argv[i+1] = (char*) NULL;
-                                                                                                                                                                                     
-
+  argv[i+1] = (char*) NULL;                                                                                                                                                                                   
   cout << "done parsing" << endl;
   cout << "num args: " << numArgs << endl;
   for (int j = 0; j < numArgs; j++)
   {
    cout << argv[j] << endl;
   }
-  
   execvp(cmd, argv);
   perror("error exec'ing");
   return 0;
